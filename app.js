11f775754eac4701f7e328dfc2f7ed6876c4d43f
mohdrafey1 <mohdrafey600@gmail.com>
2024-08-21 01:40:32 +0530

    db connect

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const Mongo_URL = 'mongodb://127.0.0.1:27017/college';
+
+main()
+    .then(() => {
+        console.log('Connected to DB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(Mongo_URL);
+}
 
 // Sample Route
 app.get('/', (req, res) => {
