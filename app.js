3d395e83bf41d5b61498df294607c06ffc641422
mohdrafey1 <mohdrafey600@gmail.com>
2024-08-23 20:50:23 +0530

    update crud for college

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const Colleges = require('./models/Colleges.js');
 const path = require('path');
+const methodOverride = require('method-override');
 
 const Mongo_URL = 'mongodb://127.0.0.1:27017/CollegeResources';
 
@@ -19,12 +20,14 @@ main();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.send('Backend is running');
 });
 
-//college route
+//college routes
+//index
 app.get('/colleges', async (req, res) => {
     try {
         const allColleges = await Colleges.find({});
@@ -34,11 +37,44 @@ app.get('/colleges', async (req, res) => {
         res.status(500).send('Error fetching colleges');
     }
 });
+//new
+app.get('/colleges/new', (req, res) => {
+    res.render('colleges/new.ejs');
+});
 
+//show
 app.get('/colleges/:id', async (req, res) => {
     let { id } = req.params;
     const college = await Colleges.findById(id);
     res.render('colleges/show.ejs', { college });
+});
+
+app.post('/colleges', async (req, res) => {
+    const newCollege = new Colleges(req.body.college);
+    await newCollege.save();
+    res.redirect('/colleges');
+    console.log('New college added:', newCollege);
+});
+
+//edit
+app.get('/colleges/:id/edit', async (req, res) => {
+    let { id } = req.params;
+    const college = await Colleges.findById(id);
+    res.render('colleges/edit.ejs', { college });
+});
+//update
+app.put('/colleges/:id', async (req, res) => {
+    let { id } = req.params;
+    await Colleges.findByIdAndUpdate(id, { ...req.body.college });
+    res.redirect(`/colleges/${id}`);
+});
+
+//delete
+app.delete('/colleges/:id', async (req, res) => {
+    let { id } = req.params;
+    let deleteCollege = await Colleges.findByIdAndDelete(id);
+    console.log(deleteCollege);
+    res.redirect('/colleges');
 });
 
 app.listen(8080, () => {
