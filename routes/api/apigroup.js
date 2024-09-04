f1d789e72278885d02dc79818afb20b50475cc68
mohdrafey1 <mohdrafey600@gmail.com>
2024-09-04 22:51:07 +0530

    api protect

--- routes/api/apigroup.js
+++ routes/api/apigroup.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const wrapAsync = require('../../utils/wrapAsync.js');
+const { validateApiKey } = require('../../middleware.js');
 
 const apiGroupController = require('../../controllers/api/group.js');
 
 // Fetch all pyq and send as JSON
-router.get('/', apiGroupController.fetchGroups);
+router.get('/', validateApiKey, apiGroupController.fetchGroups);
 
 // Create a new pyq
 router.post('/', wrapAsync(apiGroupController.createGroup));
--- routes/api/apinotes.js
+++ routes/api/apinotes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const wrapAsync = require('../../utils/wrapAsync.js');
+const { validateApiKey } = require('../../middleware.js');
 
 const apiNotesController = require('../../controllers/api/notes.js');
 
-// Fetch all NOtes and send as JSON
-router.get('/', apiNotesController.fetchNotes);
+router.get('/', validateApiKey, apiNotesController.fetchNotes);
 
 // Create a new notes
 router.post('/', wrapAsync(apiNotesController.createNotes));
