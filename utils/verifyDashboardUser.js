f222a041049159247a4b23c376addb70df30860f
mohdrafey1 <mohdrafey600@gmail.com>
2025-03-03 00:37:18 +0530

    error optimization

--- utils/verifyDashboardUser.js
+++ utils/verifyDashboardUser.js
@@ -4,10 +4,12 @@ const { errorHandler } = require('./error');
 module.exports.verifyDashboardUser = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
 
-    if (!token) {
-        return next(errorHandler(401, 'Unauthorized: No token provided'));
+    if (!token || token === 'null') {
+        console.log('Invalid Token:', token);
+        return res
+            .status(401)
+            .json({ message: 'Unauthorized: You are not logged in' });
     }
-
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
             return next(
@@ -23,7 +25,12 @@ module.exports.verifyDashboardUser = (req, res, next) => {
 module.exports.requireRole = (roles) => {
     return (req, res, next) => {
         if (!req.user || !roles.includes(req.user.role)) {
-            return res.status(403).json({ message: 'Access denied' });
+            return res
+                .status(403)
+                .json({
+                    message:
+                        'Access denied: Only Admin or Moderator can access this',
+                });
         }
         next();
     };
