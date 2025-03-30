be1eea642eef49c0cc37d663b236bcc24b15492a
mohdrafey1 <mohdrafey600@gmail.com>
2025-03-30 22:05:11 +0530

    add edit delete subject for new db

--- controllers/newdashboard/resource.controller.js
+++ controllers/newdashboard/resource.controller.js
@@ -19,7 +19,74 @@ module.exports.getBranches = async (req, res) => {
 
 module.exports.getSubjects = async (req, res) => {
     const subjects = await Subject.find({}).populate('branch').sort({
-        created: -1,
+        createdAt: -1,
     });
     res.status(200).json(subjects);
+};
+
+module.exports.addSubject = async (req, res, next) => {
+    const { subjectName, subjectCode, semester, branch } = req.body;
+
+    const existingSubject = await Subject.findOne({
+        subjectName: subjectName,
+        subjectCode: subjectCode,
+        semester: semester,
+        branch: branch,
+    });
+
+    if (existingSubject) {
+        return next(
+            errorHandler(401, 'This subject is already added in this branch')
+        );
+    }
+
+    const newSubject = new Subject({
+        subjectCode,
+        subjectName,
+        branch,
+        semester,
+    });
+
+    if (branch) {
+        const branchData = await Branch.findById(branch);
+        if (branchData) {
+            branchData.totalSubject += 1;
+            await branchData.save();
+        }
+    }
+
+    await newSubject.save();
+
+    res.json({ message: 'New Subject Added Successfully' });
+};
+
+module.exports.editSubject = async (req, res, next) => {
+    const { id } = req.params;
+    const { subjectName, subjectCode, semester } = req.body;
+
+    const updatedSubject = await Subject.findByIdAndUpdate(
+        id,
+        { subjectName, subjectCode, semester },
+        { new: true }
+    );
+
+    if (!updatedSubject) {
+        return next(errorHandler(401, 'Subject not found'));
+    }
+
+    res.json({ message: 'Subject updated successfully' });
+};
+
+module.exports.deleteSubject = async (req, res) => {
+    const { id } = req.params;
+    const subject = await Subject.findById(id);
+    if (subject) {
+        const branch = await Branch.findById(subject.branch);
+        if (branch) {
+            branch.totalSubject -= 1;
+            await branch.save();
+        }
+        await subject.deleteOne();
+    }
+    res.json({ message: 'Subject deleted successfully' });
 };
--- routes/newdashboard/resource.routes.js
+++ routes/newdashboard/resource.routes.js
@@ -6,12 +6,40 @@ const {
     getCourses,
     getBranches,
     getSubjects,
+    addSubject,
+    editSubject,
+    deleteSubject,
 } = require('../../controllers/newdashboard/resource.controller.js');
+const {
+    verifyDashboardUser,
+    requireRole,
+} = require('../../utils/verifyDashboardUser.js');
 
 router.get('/courses', wrapAsync(getCourses));
 
 router.get('/branches', wrapAsync(getBranches));
 
 router.get('/subjects', wrapAsync(getSubjects));
+
+router.post(
+    '/subjects',
+    verifyDashboardUser,
+    requireRole(['Admin', 'Moderator']),
+    wrapAsync(addSubject)
+);
+
+router.put(
+    '/subjects/:id',
+    verifyDashboardUser,
+    requireRole(['Admin', 'Moderator']),
+    wrapAsync(editSubject)
+);
+
+router.delete(
+    '/subjects/:id',
+    verifyDashboardUser,
+    requireRole(['Admin', 'Moderator']),
+    wrapAsync(deleteSubject)
+);
 
 module.exports = router;
